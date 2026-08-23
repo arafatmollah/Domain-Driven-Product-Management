d88b61041479466a546d11c0ef3c68590b153d8a
arafatmollah <arafatshuvo18@gmail.com>
2026-08-23 16:43:56 +0600

    refactor controller

--- ProductManagement/ProductManagement.API/Controllers/ProductController.cs
+++ ProductManagement/ProductManagement.API/Controllers/ProductController.cs
@@ -2,40 +2,25 @@ using Microsoft.AspNetCore.Mvc;
 using ProductManagement.DTO.Command;
 using ProductManagement.DTO.Query;
 using ProductManagement.DTO.Response;
-using ProductManagement.Handler;
 using ProductManagement.Handler.Abstraction;
 
 namespace ProductManagement.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductController : ControllerBase
+public class ProductController(
+    ICommandHandler<CreateProductCommandDto, ProductResponseDto> createHandler,
+    IQueryHandler<GetProductsQuery, IEnumerable<ProductResponseDto>> getAllHandler,
+    IQueryHandler<GetProductQuery, ProductResponseDto> getByIdHandler,
+    ICommandHandler<UpdateProductCommandDto, ProductResponseDto> updateHandler,
+    ICommandHandler<DeleteProductCommandDto, bool> deleteHandler
+) : ControllerBase
 {
-    private readonly ICommandHandler<CreateProductCommandDto, ProductResponseDto> _createHandler;
-    private readonly IQueryHandler<GetProductsQuery, IEnumerable<ProductResponseDto>> _getAllHandler;
-    private readonly IQueryHandler<GetProductQuery, ProductResponseDto> _getByIdHandler;
-    private readonly ICommandHandler<UpdateProductCommandDto, ProductResponseDto> _updateHandler;
-    private readonly ICommandHandler<DeleteProductCommandDto, bool> _deleteHandler;
-
-    public ProductController(
-        ICommandHandler<CreateProductCommandDto, ProductResponseDto> createHandler,
-        IQueryHandler<GetProductsQuery, IEnumerable<ProductResponseDto>> getAllHandler,
-        IQueryHandler<GetProductQuery, ProductResponseDto> getByIdHandler,
-        ICommandHandler<UpdateProductCommandDto, ProductResponseDto> updateHandler,
-        ICommandHandler<DeleteProductCommandDto, bool> deleteHandler)
-    {
-        _createHandler = createHandler;
-        _getAllHandler = getAllHandler;
-        _getByIdHandler = getByIdHandler;
-        _updateHandler = updateHandler;
-        _deleteHandler = deleteHandler;
-    }
-
     [HttpPost]
     public async Task<ActionResult<ProductResponseDto>> Create(
         CreateProductCommandDto request)
     {
-        var product = await _createHandler.HandleAsync(request);
+        var product = await createHandler.HandleAsync(request);
 
         return CreatedAtAction(
             nameof(GetById),
@@ -49,12 +34,13 @@ public class ProductController : ControllerBase
         [FromQuery] decimal? minPrice,
         [FromQuery] decimal? maxPrice)
     {
-        var products = await _getAllHandler.HandleAsync(new GetProductsQuery
-        {
-            Search = search,
-            MinPrice = minPrice,
-            MaxPrice = maxPrice
-        });
+        var products = await getAllHandler.HandleAsync(
+            new GetProductsQuery
+            {
+                Search = search,
+                MinPrice = minPrice,
+                MaxPrice = maxPrice
+            });
 
         return Ok(products);
     }
@@ -62,7 +48,7 @@ public class ProductController : ControllerBase
     [HttpGet("{id:int}")]
     public async Task<ActionResult<ProductResponseDto>> GetById(int id)
     {
-        var product = await _getByIdHandler.HandleAsync(
+        var product = await getByIdHandler.HandleAsync(
             new GetProductQuery { Id = id });
 
         return Ok(product);
@@ -75,7 +61,7 @@ public class ProductController : ControllerBase
     {
         request.Id = id;
 
-        var product = await _updateHandler.HandleAsync(request);
+        var product = await updateHandler.HandleAsync(request);
 
         return Ok(product);
     }
@@ -83,7 +69,7 @@ public class ProductController : ControllerBase
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> Delete(int id)
     {
-        await _deleteHandler.HandleAsync(
+        await deleteHandler.HandleAsync(
             new DeleteProductCommandDto { Id = id });
 
         return NoContent();
